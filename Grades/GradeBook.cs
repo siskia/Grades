33ee1d95ee2a0b61667771aadc13e564ef24bc2f
Siskia Erissa <SErissa@global-aero.com>
2018-08-28 14:36:07 -0400

    added base project covering up to module 6

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+            // Initialization
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            // Represents all the grades in the GradeBook
+            float sum = 0; 
+            foreach(float grade in grades)
+            {
+                // Will execute once for each grade in the grades collection
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade; // Same as sum = sum + grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if(!String.IsNullOrEmpty(value))
+                {
+                    if(_name != value)
+                    {
+                        NameChangedEventArgs args = new NameChangedEventArgs();
+                        args.ExistingName = _name;
+                        args.NewName = value;
+
+                        // Passing along myself
+                        NameChanged(this, args);
+                    }
+                    _name = value;
+                }
+            }
+        }
+
+        public event NameChangedDelegate NameChanged;
+
+        private string _name;
+
+        // Using Systems.Collections.Generic
+        private List<float> grades;
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the variable
+            GradeBook book = new GradeBook();
+
+            book.NameChanged += OnNameChanged;
+
+            // Assignment to the Name field in GradeBook.cs
+            book.Name = "Kia's Grade Book";
+            book.Name = "Grade Book";
+           
+            book.AddGrade(91);
+            // Double to float
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{ description}: { result:F2}");
+        }
+    }
+}
